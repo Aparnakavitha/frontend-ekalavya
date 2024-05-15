3bd07541e61f2f4e1dbc54717e2d1aade6dceda5
Nazeem-c <mhdnazeemc@gmail.com>
2024-05-15 18:43:17 +0530

    template code

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Hello Super21</h3>
+        <h1>Lets start</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
