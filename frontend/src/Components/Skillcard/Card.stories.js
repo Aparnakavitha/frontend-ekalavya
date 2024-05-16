d9356f06cbf41265772d85c5ba5b74e6c099d8ef
prasanth6666 <prasanthjmathew@gmail.com>
2024-05-16 11:37:03 +0530

    initial commit

---
+++ frontend/src/Components/Skillcard/Card.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './SkillCard';
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  argTypes: {
+    title: { control: 'text' },
+    subtitle: { control: 'text' },
+    showCloseIcon: { control: 'boolean' },
+    onClose: { action: 'Close button clicked' }, // Define action for onClose prop
+  },
+};
+
+const Template = (args) => <Card {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "Java",
+  subtitle: "Level 1",
+  showCloseIcon: true,
+  onClose: () => {
+    // Define functionality for close icon click
+    console.log("Close button clicked");
+    // You can add additional functionality here
+  },
+};
