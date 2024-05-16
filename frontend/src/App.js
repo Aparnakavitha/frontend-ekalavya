4211743fd1687fdc61903fb71916ee41942f5255
shesnajalal <shesna.jalal@tarento.com>
2024-05-16 12:13:15 +0530

    InputBox component4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
 
 import './App.css';
-import './Components/InputBox/InputBox';
-
-import Input from './Components/InputBox/InputBox';
 
 function App() {
-  const inputData = {
-    label: 'Enter email addresses', 
-    width: "800px",
-    height: "57px",
-    placeholders: ['Address 1', 'Address 2', 'Address 3'] 
-}
   return (
     <div className="App">
       <header className="App-header">
         <h3>Hello Super21</h3>
         <h1>Lets start</h1>
-        <Input{...inputData}/>
       </header>
     </div>
   );
