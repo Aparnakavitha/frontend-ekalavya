cb94ef238c3c7eaa91df1f4e0a3df348daf2e6c2
aleenajayan <aleenajayan2001@gmail.com>
2024-05-16 12:40:24 +0530

    filter second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
-import Filter from './Components/Filter/Filter';
 import './App.css';
 
 function App() {
-  const handleDropClick = (selectedOption) => {
-    console.log("Selected option:", selectedOption);
-  };
-
-  const heading = "q";
-  const content = [
-    "rc",
-    "",
-    "",
-    "",
-  ];
-
   return (
     <div className="App">
-      {/* <h1>My Application</h1> */}
-      <Filter Heading={heading} Content={content} handleDropClick={handleDropClick} />
+      <header className="App-header">
+        <h3>Hello Super21</h3>
+        <h1>Lets start</h1>
+      </header>
     </div>
   );
 }
+
 export default App;
