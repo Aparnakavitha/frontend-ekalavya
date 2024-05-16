b5a44a8d6ddcfca08d0354a94f1977f6db2bbe12
Aiswarya <aiswarya.velayudhan@tarento.com>
2024-05-16 12:48:11 +0530

    profilepic

---
+++ frontend/src/Components/ProfilePicture/ProfilePicture.js
@@ -0,0 +1,31 @@
+// ProfilePicture.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ProfilePicture.module.css'; // Import your CSS module
+
+const ProfilePicture = ({ src, size }) => {
+  const getSizeClassName = () => {
+    return size === 'small' ? styles['profile-picture-frame-small'] : styles['profile-picture-frame'];
+  };
+
+  const getPictureSizeClassName = () => {
+    return size === 'small' ? styles['profile-picture-small'] : styles['profile-picture'];
+  };
+
+  return (
+    <div className={getSizeClassName()}>
+      <img src={src} alt="Profile" className={getPictureSizeClassName()} />
+    </div>
+  );
+};
+
+ProfilePicture.propTypes = {
+  src: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['big', 'small']),
+};
+
+ProfilePicture.defaultProps = {
+  size: 'big',
+};
+
+export default ProfilePicture;
