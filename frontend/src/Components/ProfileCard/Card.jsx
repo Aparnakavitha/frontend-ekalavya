03dd9e396408bde0afb0576fefdae13aa086b3e5
HannaAnn <hanna.joseph@tarento.com>
2024-05-21 13:03:50 +0530

    sample

--- frontend/src/Components/ProfileCard/Card.jsx
+++ frontend/src/Components/ProfileCard/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-const Card=(props)=> {
+const Card = (props) => {
   const {
     profileImage,
     title1,
@@ -13,18 +13,18 @@ const Card=(props)=> {
   } = props;
 
   return (
-    <div className={styles.card} onClick={onClick}> 
-      <div className={styles.cardContent}> 
+    <div className={styles.card} onClick={onClick}>
+      <div className={styles.cardContent}>
         <div className={styles.profile}>
-          <img src={profileImage} alt="Profile" className={styles.profileImage} /> 
+          <img src={profileImage} alt="Profile" className={styles.profileImage} />
         </div>
-        <div className={styles.content}> 
-          <div className={styles.titlesWrapper}> 
-            <h2 className={`${styles.title} ${styles.title1}`}>{title1}</h2> 
-            <h3 className={`${styles.title} ${styles.title2}`}>{title2}</h3> 
-            <h4 className={`${styles.title} ${styles.title3}`}>{title3}</h4> 
+        <div className={styles.content}>
+          <div className={styles.titlesWrapper}>
+            <h2 className={`${styles.title} ${styles.title1}`}>{title1}</h2>
+            <h3 className={`${styles.title} ${styles.title2}`}>{title2}</h3>
+            <h4 className={`${styles.title} ${styles.title3}`}>{title3}</h4>
           </div>
-          <div className={styles.contactInfo}> 
+          <div className={styles.contactInfo}>
             <p>{email}</p>
             <p>{phone}</p>
           </div>
