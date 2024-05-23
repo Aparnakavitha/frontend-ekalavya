5d9381dcf340a5411051114c9cfeda10d4f42f19
Thejkiran <thej.kiran@tarento.com>
2024-05-23 15:29:52 +0530

    Components ready for merge

--- frontend/src/layouts/studentprofile/components/Upcoming.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Upcoming from "./Upcoming";
-
-export default {
-  title: "Layouts/Upcoming events",
-  component: Upcoming,
-};
-
-export const UpcomingDark = () => <Upcoming />;
