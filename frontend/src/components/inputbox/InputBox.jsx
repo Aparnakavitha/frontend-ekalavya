f444755679096a03a40f279322acf4347735873f
aswanthkanappally <aswanth.kanappally@tarento.com>
2024-05-24 15:28:42 +0530

    Updateds

--- frontend/src/components/inputbox/InputBox.jsx
+++ frontend/src/components/inputbox/InputBox.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { FaRegImage } from "react-icons/fa";
 import styles from "./InputBox.module.css";
-
-const InputBox = ({
+ 
+const Input = ({
   size,
   label,
   placeholders,
@@ -86,21 +86,6 @@ const InputBox = ({
                 className={`${styles.input} ${styles["large-size"]} ${styles["file-input"]}`}
               />
             </div>
-          ) : isDropdown ? (
-            <select
-              {...rest}
-              value={value}
-              onChange={handleSelectChange}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-              className={`${styles.input} ${size === "normal" ? styles["normal-size"] : ""} ${size === "small" ? styles["small-size"] : ""}`}
-            >
-              {options.map((option, index) => (
-                <option key={index} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
           ) : size === "large" ? (
             <textarea
               {...rest}
@@ -127,5 +112,5 @@ const InputBox = ({
     </div>
   );
 };
-
-export default InputBox;
+ 
+export default Input;
