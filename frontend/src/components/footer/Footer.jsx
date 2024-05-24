4c9179ea8026efd3b14d05e87a4384d648e1b016
NazeemC <mohammed.nazeem@tarento.com>
2024-05-24 11:53:19 +0530

    final team2

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -20,7 +20,6 @@ const Footer = (props) => {
           <span className={styles.quote}>{quoteContent}</span>
         </div>
       </div>
-
       <div className={styles.bottombox}>
         <div className={copyrightClass}>
           <span className={styles.copyrightContent}>{copyrightContent}</span>
