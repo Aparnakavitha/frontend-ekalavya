94e0ca578ddb4f3b12de162cfc174f6b1856e1aa
NazeemC <mohammed.nazeem@tarento.com>
2024-05-29 19:19:03 +0530

    Common layout is checked

--- frontend/src/layouts/common/components/Footer.stories.js
+++ frontend/src/layouts/common/components/Footer.stories.js
@@ -2,26 +2,26 @@ import Footer from "./Footer";
 import image from "../../../assets/Logo.png";
 
 export default {
-    title: "Layouts/Common/Components/Footer",
-    component: Footer,
+  title: "Layouts/Common/Components/Footer",
+  component: Footer,
 };
 
 export const FooterCentreAligned = {
-    args: {
-        Logo: image,
-        quoteContent: "Embark on Your Learning Journey Today!",
-        copyrightContent: "All rights reserved © 2024 Tarento Group.",
-        copyrightContent2: " | Privacy Policy",
-        isLeftALigned: false,
-    },
+  args: {
+    Logo: image,
+    quoteContent: "Embark on Your Learning Journey Today!",
+    copyrightContent: "All rights reserved © 2024 Tarento Group.",
+    copyrightContent2: " | Privacy Policy",
+    isLeftALigned: false,
+  },
 };
 
 export const FooterLeftAligned = {
-    args: {
-        Logo: image,
-        quoteContent: "Embark on Your Learning Journey Today!",
-        copyrightContent: "All rights reserved © 2024 Tarento Group.",
-        copyrightContent2: " | Privacy Policy",
-        isLeftALigned: true,
-    },
+  args: {
+    Logo: image,
+    quoteContent: "Embark on Your Learning Journey Today!",
+    copyrightContent: "All rights reserved © 2024 Tarento Group.",
+    copyrightContent2: " | Privacy Policy",
+    isLeftALigned: true,
+  },
 };
