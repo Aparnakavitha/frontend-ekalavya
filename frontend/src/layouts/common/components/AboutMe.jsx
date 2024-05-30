0e5f8acf314de76e8e46f3c666ac10ac726881fe
Thejkiran <thej.kiran@tarento.com>
2024-05-30 10:21:38 +0530

    Added AboutMe layout under common layouts

--- frontend/src/layouts/common/components/AboutMe.jsx
+++ frontend/src/layouts/common/components/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styles from "../AboutMe.module.css";
+import styles from "../Common.module.css";
 
 const AboutMe = ({ title, description }) => {
   return (
