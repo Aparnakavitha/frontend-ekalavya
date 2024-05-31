d00d66429a436bf90b2087a4a18c8f21a1598545
adilparayil <adil.parayil@tarento.com>
2024-05-31 17:59:55 +0530

    edits

--- frontend/src/layouts/admin-skill/components/AddSkill.stories.js
+++ frontend/src/layouts/admin-skill/components/AddSkill.stories.js
@@ -2,11 +2,11 @@ import React from "react";
 import AddSkill from "./AddSkill";
 
 export default {
-  title: "layouts/AdminSkill/Components/AddSkill",
+  title: "layouts/AdminSkill/Components/Add Skill",
   component: AddSkill,
 };
 
 const Template = (args) => <AddSkill {...args} />;
 
-export const addSkill = Template.bind({});
-addSkill.args = {};
+export const Addskill = Template.bind({});
+Addskill.args = {};
