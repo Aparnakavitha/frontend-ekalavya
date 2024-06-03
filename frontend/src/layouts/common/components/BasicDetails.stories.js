deb677a2832e66ea66bc0d0a722593872a6d920a
geojoseph <geo.joseph@tarento.com>
2024-06-03 15:07:23 +0530

    team3 modal layouts added

---
+++ frontend/src/layouts/common/components/BasicDetails.stories.js
@@ -0,0 +1,25 @@
+import React from "react";
+import BasicDetails from "./BasicDetails";
+
+export default {
+  title: "Layouts/Common/Components/Edit Basic Details ",
+  component: BasicDetails,
+};
+
+const Template = (args) => <BasicDetails {...args} />;
+
+export const Editbasicdetails = Template.bind({});
+
+Editbasicdetails.args = {
+  mainHeading: "Edit Basic Details",
+  initialData: {
+    dob: "1990-01-01",
+    phoneNumber: "1234567890",
+    houseName: "Sample House",
+    city: "Sample City",
+    pinCode: "123456",
+    state: "Sample State",
+    country: "Sample Country",
+    aboutMe: "This is a sample about me",
+  },
+};
