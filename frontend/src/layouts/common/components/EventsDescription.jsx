990ffa711380d04a9d1aafa777b5b68d0e02484d
NazeemC <mohammed.nazeem@tarento.com>
2024-06-04 16:41:00 +0530

    Events Description

--- frontend/src/layouts/common/components/EventsDescription.jsx
+++ frontend/src/layouts/common/components/EventsDescription.jsx
@@ -5,15 +5,18 @@ import PrimaryButton from "../../../components/buttons/PrimaryButton";
 
 const EventsDescription = (props) => {
   const {
-    heading,
-    text,
-    texts,
-    desc,
-    date,
-    time,
-    venue,
-    address,
+    eventTitle,
+    eventType,
+    eventMode,
+    description,
+    startDate,
+    endDate,
+    startTime,
+    endTime,
+    location,
     speaker,
+    speakerDescription,
+    organizer,
     button,
     buttons,
     small,
@@ -23,8 +26,24 @@ const EventsDescription = (props) => {
     smaller,
     onclick1,
     onclick2,
-    onclick3
+    onclick3,
   } = props;
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const month = date.toLocaleDateString("en-US", { month: "short" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+
+    return `${month} ${day}, ${year}`;
+  }
+
+  function formatTime(timeString) {
+    const [hours, minutes] = timeString.split(":");
+    const convertedHours = hours % 12 || 12;
+    const amPm = hours < 12 ? "AM" : "PM";
+    return `${convertedHours.toString().padStart(2, "0")}:${minutes} ${amPm}`;
+  }
 
   return (
     <div className={`${styles["eventsdescription-container"]}`}>
@@ -34,7 +53,7 @@ const EventsDescription = (props) => {
             <NavButton pageName={button} />
           </div>
           <div>
-            <NavButton pageName={buttons} onClick={onclick1}/>
+            <NavButton pageName={buttons} onClick={onclick1} />
           </div>
         </div>
       </div>
@@ -45,16 +64,16 @@ const EventsDescription = (props) => {
             <div className={`${styles["eventsdescription-content"]}`}>
               <div>
                 <h2 className={`${styles["eventsdescription-contentheading"]}`}>
-                  {heading}
+                  {eventTitle}
                 </h2>
               </div>
               <div className={`${styles["eventsdescription-texted"]}`}>
                 <a className={`${styles["eventsdescription-text"]}`}>
                   <b> Type : </b>
-                  {text}
+                  {eventType}
                 </a>
                 <a className={`${styles["eventsdescription-texts"]}`}>
-                  Event Mode : {texts}
+                  Event Mode : {eventMode}
                 </a>
               </div>
             </div>
@@ -62,9 +81,15 @@ const EventsDescription = (props) => {
             {type === "public" && (
               <>
                 <div className={`${styles["eventsdescription-primarydiv"]}`}>
-                  <div className={`${styles["eventsdescription-primarybutton"]}`}>
+                  <div
+                    className={`${styles["eventsdescription-primarybutton"]}`}
+                  >
                     <div>
-                      <PrimaryButton content={smaller} variant="secondary" onClick={onclick1}/>
+                      <PrimaryButton
+                        content={smaller}
+                        variant="secondary"
+                        onClick={onclick1}
+                      />
                     </div>
                   </div>
                 </div>
@@ -74,9 +99,15 @@ const EventsDescription = (props) => {
             {type === "mentor" && (
               <>
                 <div className={`${styles["eventsdescription-primarydiv"]}`}>
-                  <div className={`${styles["eventsdescription-primarybutton"]}`}>
+                  <div
+                    className={`${styles["eventsdescription-primarybutton"]}`}
+                  >
                     <div>
-                      <PrimaryButton content={small} variant="secondary" onClick={onclick1}/>
+                      <PrimaryButton
+                        content={small}
+                        variant="secondary"
+                        onClick={onclick1}
+                      />
                     </div>
                   </div>
                 </div>
@@ -87,9 +118,21 @@ const EventsDescription = (props) => {
               <>
                 <div>
                   <div className={`${styles["eventsdescription-primarydiv"]}`}>
-                    <PrimaryButton content={large} variant="secondary" onClick={onclick1} />
-                    <PrimaryButton content={medium} variant="secondary" onClick={onclick2}/>
-                    <PrimaryButton content={small} variant="secondary" onClick={onclick3}/>
+                    <PrimaryButton
+                      content={large}
+                      variant="secondary"
+                      onClick={onclick1}
+                    />
+                    <PrimaryButton
+                      content={medium}
+                      variant="secondary"
+                      onClick={onclick2}
+                    />
+                    <PrimaryButton
+                      content={small}
+                      variant="secondary"
+                      onClick={onclick3}
+                    />
                   </div>
                 </div>
               </>
@@ -101,7 +144,7 @@ const EventsDescription = (props) => {
               <h3>Description</h3>
             </div>
             <div className={`${styles["eventsdescription-desc"]}`}>
-              <p>{desc}</p>
+              <p>{description}</p>
             </div>
           </div>
 
@@ -112,12 +155,12 @@ const EventsDescription = (props) => {
               </div>
               <div className={`${styles["eventsdescription-timer"]}`}>
                 <a className={`${styles["eventsdescription-date"]}`}>
-                  • <b>Date :</b>
-                  {date}
+                  • <b>Date : </b>
+                  {formatDate(startDate)} - {formatDate(endDate)}
                 </a>
                 <a className={`${styles["eventsdescription-time"]}`}>
-                  • <b>Time :</b>
-                  {time}
+                  • <b>Time : </b>
+                  {formatTime(startTime)} - {formatTime(endTime)}
                 </a>
               </div>
             </div>
@@ -128,10 +171,7 @@ const EventsDescription = (props) => {
               </div>
               <div className={`${styles["eventsdescription-timer"]}`}>
                 <a className={`${styles["eventsdescription-venue"]}`}>
-                  • <b>Venue :</b> {venue}
-                </a>
-                <a className={`${styles["eventsdescription-address"]}`}>
-                  • <b>Address :</b> {address}
+                  • <b>Venue :</b> {location}
                 </a>
               </div>
             </div>
@@ -142,20 +182,24 @@ const EventsDescription = (props) => {
               <h3>Speakers</h3>
             </div>
             <div>
-              <a className={`${styles["eventsdescription-speaker"]}`}>• {speaker}</a>
+              <a className={`${styles["eventsdescription-speaker"]}`}>
+                • <b>{speaker} </b>, {speakerDescription}
+              </a>
             </div>
           </div>
 
           {(type === "public" || type === "admin") && (
             <>
               <div>
-                <div className={`${styles["eventsdescription-headingcontent"]}`}>
+                <div
+                  className={`${styles["eventsdescription-headingcontent"]}`}
+                >
                   <h3>Organizer</h3>
                 </div>
 
                 <div>
                   <a className={`${styles["eventsdescription-speaker"]}`}>
-                    • {speaker}
+                    • {organizer}
                   </a>
                 </div>
               </div>
