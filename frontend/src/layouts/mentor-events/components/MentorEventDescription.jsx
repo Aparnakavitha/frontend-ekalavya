3037aebb7b01e4fe8aea6cbe5c1a7f38207d56e8
vadhi <vadhiraj.taribalilu@tarento.com>
2024-06-04 13:22:20 +0530

    modal done

--- frontend/src/layouts/mentor-events/components/MentorEventDescription.jsx
+++ frontend/src/layouts/mentor-events/components/MentorEventDescription.jsx
@@ -3,36 +3,39 @@ import EventsDescription from "../../common/components/EventsDescription";
 import EventDescriptionData from "./EventDescriptionData";
 import Modal from "../../common/components/Modal";
 import AddEvent from "../../admin-event/components/AddEvent";
- 
+
 const MentorEventDescription = () => {
   const [isOpen, setIsOpen] = useState(false);
- 
+
   const handleOpenModal = () => {
     setIsOpen(true);
   };
- 
+
   const handleCloseModal = () => {
     setIsOpen(false);
   };
- 
+
   const handleFormSubmit = (formData) => {
     console.log("Form Submitted with data:", formData);
     handleCloseModal();
   };
- 
+
   const actionData = {
     ...EventDescriptionData,
     onclick1: handleOpenModal,
   };
- 
+
   return (
-<div>
-<EventsDescription {...actionData} />
-<Modal isOpen={isOpen} widthVariant="large" onClose={handleCloseModal}>
-<AddEvent {...EventDescriptionData.addeventprops} onSubmit={handleFormSubmit} />
-</Modal>
-</div>
+    <div>
+      <EventsDescription {...actionData} />
+      <Modal isOpen={isOpen} widthVariant="large" onClose={handleCloseModal}>
+        <AddEvent
+          {...EventDescriptionData.addeventprops}
+          onSubmit={handleFormSubmit}
+        />
+      </Modal>
+    </div>
   );
 };
- 
+
 export default MentorEventDescription;
