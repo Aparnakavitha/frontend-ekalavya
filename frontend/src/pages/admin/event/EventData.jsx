43d611fca324b5d4fc9888a09a078eb9e497791e
geojoseph <geo.joseph@tarento.com>
2024-06-06 13:50:38 +0530

    Admin-Event page added

---
+++ frontend/src/pages/admin/event/EventData.jsx
@@ -0,0 +1,556 @@
+import image from "../../../assets/DP.png";
+
+const EventData = {
+  greetingData: {
+    welcome: "Welcome Back",
+    name: "John",
+    info: "Here is the information about",
+    profile: "Students",
+    showButtons: true,
+    viewprops: {
+      data: [
+        [
+          "CLG001",
+          "Massachusetts Institute of Technology",
+          "Cambridge",
+          "Massachusetts",
+          "USA",
+        ],
+        ["CLG002", "Stanford University", "Stanford", "California", "USA"],
+        ["CLG003", "Harvard University", "Cambridge", "Massachusetts", "USA"],
+        [
+          "CLG004",
+          "California Institute of Technology",
+          "Pasadena",
+          "California",
+          "USA",
+        ],
+        ["CLG005", "University of Oxford", "Oxford", "Oxfordshire", "UK"],
+        [
+          "CLG006",
+          "University of Cambridge",
+          "Cambridge",
+          "Cambridgeshire",
+          "UK",
+        ],
+        ["CLG007", "Imperial College London", "London", "Greater London", "UK"],
+        ["CLG008", "ETH Zurich", "Zurich", "Zurich", "Switzerland"],
+        ["CLG009", "University of Chicago", "Chicago", "Illinois", "USA"],
+        [
+          "CLG010",
+          "National University of Singapore",
+          "Singapore",
+          "Singapore",
+          "Singapore",
+        ],
+        ["CLG011", "Princeton University", "Princeton", "New Jersey", "USA"],
+        ["CLG012", "Yale University", "New Haven", "Connecticut", "USA"],
+        ["CLG013", "Columbia University", "New York City", "New York", "USA"],
+        [
+          "CLG014",
+          "University of California, Berkeley",
+          "Berkeley",
+          "California",
+          "USA",
+        ],
+        [
+          "CLG015",
+          "University of Pennsylvania",
+          "Philadelphia",
+          "Pennsylvania",
+          "USA",
+        ],
+        [
+          "CLG016",
+          "University of California, Los Angeles",
+          "Los Angeles",
+          "California",
+          "USA",
+        ],
+        [
+          "CLG017",
+          "University College London",
+          "London",
+          "Greater London",
+          "UK",
+        ],
+        ["CLG018", "University of Toronto", "Toronto", "Ontario", "Canada"],
+        ["CLG019", "Cornell University", "Ithaca", "New York", "USA"],
+        ["CLG020", "Tsinghua University", "Beijing", "Beijing", "China"],
+      ],
+      headings: ["ClgID", "CollegeName", "Place", "State", "Country"],
+    },
+    addprops: {},
+  },
+  dataView: {
+    data: [
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+      {
+        miniHeading: "Capstone",
+        mainHeading: "Health Management",
+        startDate: "Jan 15, 2030",
+        endDate: "Mar 15, 2030",
+        description:
+          "Unlock the power of data with our comprehensive Introduction to Data",
+        cardType: "Course",
+        handleClick: (e) => {
+          console.log("clicked");
+        },
+      },
+    ],
+    tableColumns: [
+      { key: "miniHeading", displayName: "Type" },
+      { key: "mainHeading", displayName: "Title" },
+      { key: "startDate", displayName: "Start Date" },
+      { key: "endDate", displayName: "End Date" },
+      { key: "description", displayName: "Description" },
+    ],
+    toggle: true,
+    itemsPerPage: 15,
+  },
+};
+
+export default EventData;
