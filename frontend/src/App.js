6fbe0df4daeccf2ec971ccbe26074297ee5e1689
Sneha Ravindran <sneha.ravindran@tarento.com>
2024-06-06 16:51:42 +0530

    updated app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,12 @@
 import "./App.css";
-import Profile from "./pages/student/student-profile/Profile";
-import SkillLayout from "./layouts/student-skill/components/SkillLayout";
-import StudentEvent from "./pages/student/student-events/StudentEvents";
 
 function App() {
   return (
     <div>
       <div className="App">
         <header className="App-header">
-          {/* <h3>Hello Super21</h3>
-          <h1>Lets start</h1> */}
-          {/* <Profile /> */}
-          <SkillLayout />
-          {/* <StudentEvent /> */}
+          <h3>Hello Super21</h3>
+          <h1>Lets start</h1>
         </header>
       </div>
     </div>
@@ -20,4 +14,3 @@ function App() {
 }
 
 export default App;
-
