ac5be6bd0d0dda33d3752a49803f6fd0ff2138aa
adilparayil <adil.parayil@tarento.com>
2024-06-07 14:24:23 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+
 function App() {
   return (
     <div>
       <div className="App">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <h3>Hello Super21</h3>
+          <h1>Lets start</h1>
+        </header>
       </div>
     </div>
   );
