abc045093bbd899ea7b54a42d4ee42c254f97445
geojoseph <geo.joseph@tarento.com>
2024-06-10 12:29:49 +0530

    Admin page update

--- frontend/src/pages/admin/AdminAtoms.js
+++ frontend/src/pages/admin/AdminAtoms.js
@@ -1,6 +1,6 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 export const currentPageState = atom({
-  key: 'currentPageState',
-  default: 'Student',
+  key: "currentPageState",
+  default: "student",
 });
