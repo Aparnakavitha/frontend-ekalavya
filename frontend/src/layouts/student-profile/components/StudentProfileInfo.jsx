824aaaa1bd1826be52386a583cb3f3463a464b1d
Thejkiran <thej.kiran@tarento.com>
2024-06-12 18:28:59 +0530

    Pulling from development-test

--- frontend/src/layouts/student-profile/components/StudentProfileInfo.jsx
+++ frontend/src/layouts/student-profile/components/StudentProfileInfo.jsx
@@ -54,19 +54,7 @@ const StudentProfileInfo = (props) => {
 
   const editBox = {
     mainHeading: "Edit Basic Details",
-    initialData: {
-      profilepicture: sample.profilepicture,
-      name: sample.name,
-      dob: sample.dob,
-      college: sample.college,
-      phoneNumber: sample.phoneNumber,
-      houseName: "Skyline villa",
-      city: "Bengaluru",
-      pinCode: "795432",
-      state: "Karnataka",
-      country: "India",
-      aboutMe: "Mumble mumble mumble",
-    },
+    initialData: { ...sample },
     isEdit: true,
     onSubmit: handleFormSubmit,
   };
