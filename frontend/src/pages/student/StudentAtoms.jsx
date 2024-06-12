4a9bf002b3a8a6e5c88c1df17bd3f23a9282ada5
adilparayil <adil.parayil@tarento.com>
2024-06-12 18:04:32 +0530

    updates

--- frontend/src/pages/student/StudentAtoms.jsx
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD:frontend/src/pages/admin/AdminAtoms.js
-=======
-import { atom } from "recoil";
-
-export const currentPageState = atom({
-  key: "currentPageState",
-  default: "profile",
-});
->>>>>>> e9757e4cdc639a9768493cfee3869bbbb8480d15:frontend/src/pages/student/StudentAtoms.jsx
