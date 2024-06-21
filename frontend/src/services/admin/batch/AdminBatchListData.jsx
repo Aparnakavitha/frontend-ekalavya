a5e6887357d3decca5bfd45e2f9790803a1fb57c
shesnajalal <shesna.jalal@tarento.com>
2024-06-21 14:50:16 +0530

    AdminBatchListData.jsx updated

--- frontend/src/services/admin/batch/AdminBatchListData.jsx
+++ frontend/src/services/admin/batch/AdminBatchListData.jsx
@@ -1,231 +1,202 @@
-// service
-import axios from "axios";
-const API_URL = "http://localhost:8888";
-
-export const fetchbatches = async (params) => {
-  try {
-    const response = await axios.get(`${API_URL}/batches`, {
-      params: params,
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching batches:", error);
-    throw error;
-  }
+const AdminBatchListData = {
+  greeting: {
+    welcome: "Welcome Back",
+    name: "John",
+    info: "Here is the information about",
+    profile: "Batches",
+    showButtons: false,
+  },
+  dataView: {
+    data: [
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "batch",
+      },
+      {
+        miniHeading: "B301",
+        mainHeading: "Batch 1",
+        Count: 28,
+        cardType: "Batch",
+      },
+    ],
+    tableColumns: [
+      { key: "miniHeading", displayName: "Batch ID" },
+      { key: "mainHeading", displayName: "Batch Name" },
+      { key: "Count", displayName: "Count" },
+    ],
+    toggle: true,
+    itemsPerPage: 12,
+  },
 };
 
-
-
-const apiData = {
-  batchName: null,
-  participantCount: null,
-  batchId: null
-};
-
-
-
-// const AdminBatchListData = {
-//   greeting: {
-//     welcome: "Welcome Back",
-//     name: "John",
-//     info: "Here is the information about",
-//     profile: "Batches",
-//     showButtons: false,
-//   },
-//   dataView: {
-    // data: [
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "batch",
-    //   },
-    //   {
-    //     miniHeading: "B301",
-    //     mainHeading: "Batch 1",
-    //     Count: 28,
-    //     cardType: "Batch",
-    //   },
-    // ],
-//     tableColumns: [
-//       { key: "miniHeading", displayName: "Batch ID" },
-//       { key: "mainHeading", displayName: "Batch Name" },
-//       { key: "Count", displayName: "Count" },
-//     ],
-//     toggle: true,
-//     itemsPerPage: 12,
-//   },
-// };
-
-// export default AdminBatchListData;
-
-
-
+export default AdminBatchListData;
