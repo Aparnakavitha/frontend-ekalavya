841c2ebca5721f538bd4abd9ec7de63650840ae9
Adhilafarsana <adhila.farsana@tarento.com>
2024-06-28 16:49:51 +0530

    View skills of students in mentor

--- frontend/src/services/Skills.jsx
+++ frontend/src/services/Skills.jsx
@@ -106,7 +106,8 @@ export const getUsersCountForSkill = async (skillId) => {
 
 export const getSkillsForUser = async (userId) => {
   try {
-    const response = await axios.get(`${BASE_URL}?userId=${userId}`);
+    const type = userId ? "" : "all";
+    const response = await axios.get(`${BASE_URL}?userId=${userId}&type=${type}`);
 
     // Check if response data is an array
     if (Array.isArray(response.data)) {
