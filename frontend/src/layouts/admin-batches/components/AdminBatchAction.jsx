4ab9d2fec5bd66d6e526dbd548c64dd0a422dabf
aryasulija <arya.sulija@tarento.com>
2024-07-01 16:51:44 +0530

    Batch added frontly

--- frontend/src/layouts/admin-batches/components/AdminBatchAction.jsx
+++ frontend/src/layouts/admin-batches/components/AdminBatchAction.jsx
@@ -4,40 +4,58 @@ import Modal from "../../common/components/Modal";
 import AdminBatchActionData from "./BatchActionData";
 import BatchOperations from "./BatchOperations";
 import { createBatch } from "../../../services/Batch";
-
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+ 
+ 
 const AdminBatchAction = ({
   onSearchChange,
   batchData,
-  setBatchData,
-  setChanged,
+  setBatchData
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [submitError, setSubmitError] = useState("");
-
+  const navigate = useNavigate();
+ 
   const handleOpenModal = () => {
     setSubmitError("");
     setIsOpen(true);
   };
-
+ 
   const handleCloseModal = () => {
     setSubmitError("");
     setIsOpen(false);
   };
-
+ 
   const handleFormSubmit = async (formData) => {
     try {
       const { batchName } = formData;
       const response = await createBatch({ batchName: batchName });
-      setBatchData([...(batchData || []), response[0]]);
-      setChanged((prev) => !prev);
+      const newBatch = {
+        miniHeading: `B${response[0].batchId}`,
+        mainHeading: response[0].batchName || "",
+        Count: response[0].participantCount,
+        cardType: "batch",
+        showCount: true,
+        handleClick: () => handleClick(response[0].batchId, response[0].batchName),
+      };
+    
+      setBatchData([newBatch, ...(batchData || [])]);
       setIsOpen(false);
       setSubmitError("");
+      toast.success("Batch created successfully!");
     } catch (error) {
+      toast.error("Error adding batch!");
       console.error("Error adding batch:", error);
       setSubmitError("Batch name already exists");
     }
   };
-
+  const handleClick = (batchId, batchName) => {
+    navigate(`/admin/batches/batch-details/${batchId}`, {
+      state: { batchName },
+    });
+  };
+ 
   const actionData = {
     ...AdminBatchActionData,
     buttonProps: {
@@ -45,7 +63,7 @@ const AdminBatchAction = ({
       onClick: handleOpenModal,
     },
   };
-
+ 
   return (
     <div>
       <ActionComponent {...actionData} onSearchChange={onSearchChange} />
@@ -59,5 +77,5 @@ const AdminBatchAction = ({
     </div>
   );
 };
-
+ 
 export default AdminBatchAction;
