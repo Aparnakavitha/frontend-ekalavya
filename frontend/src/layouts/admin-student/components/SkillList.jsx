9a39322d898b5a2b776c1070015f34f9dfafbdfd
Thejkiran <thej.kiran@tarento.com>
2024-07-04 14:36:24 +0530

    Fixed skill data mixup bug in Admin-student-skill

--- frontend/src/layouts/admin-student/components/SkillList.jsx
+++ frontend/src/layouts/admin-student/components/SkillList.jsx
@@ -52,9 +52,11 @@ const SkillList = ({ studentId }) => {
             setStudentSkills(skills);
           } else {
             console.error("Unexpected response format:", response);
+            setStudentSkills([]);
           }
         } catch (error) {
           console.error("Error fetching student skills:", error);
+          setStudentSkills([]);
         }
       }
     };
@@ -153,9 +155,13 @@ const SkillList = ({ studentId }) => {
       <Modal isOpen={isOpen} widthVariant="medium" onClose={handleCloseModal}>
         <CombinedSkillForm {...addSkill} onSubmit={handleFormSubmit} />
       </Modal>
-     {studentSkills.length>0 ? (<CardRow {...skillcards} userId={studentId} />): (
-      <p className="nodata padding padding-top padding-bottom" >No skills achieved yet</p>
-     )}
+      {studentSkills.length > 0 ? (
+        <CardRow {...skillcards} userId={studentId} />
+      ) : (
+        <p className="nodata padding padding-top padding-bottom">
+          No skills achieved yet
+        </p>
+      )}
       <ToastContainer
         position="top-center"
         autoClose={5000}
