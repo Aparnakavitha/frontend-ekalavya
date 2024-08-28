42b13fab8922be1b85d0ce0fa2ee8cf83a7d1eeb
NazeemC <mohammed.nazeem@tarento.com>
2024-08-28 20:42:09 +0530

    refactor:router file

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,19 +1,21 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import secureLocalStorage from 'react-secure-storage';
-import { ToastContainer, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import secureLocalStorage from "react-secure-storage";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Home = React.lazy(() => import('../pages/index'));
-const Explore = React.lazy(() => import('../pages/Explore'));
-const EventDescription = React.lazy(() => import('../pages/EventDescription'));
-const MentorContent = React.lazy(() => import('../pages/mentor/Mentor'));
-const AdminContent = React.lazy(() => import('../pages/admin/Admin'));
-const StudentContent = React.lazy(() => import('../pages/student/Student'));
-const NotFound = React.lazy(() => import('../layouts/common/components/NotFound'));
+const Home = React.lazy(() => import("../pages/index"));
+const Explore = React.lazy(() => import("../pages/Explore"));
+const EventDescription = React.lazy(() => import("../pages/EventDescription"));
+const MentorContent = React.lazy(() => import("../pages/mentor/Mentor"));
+const AdminContent = React.lazy(() => import("../pages/admin/Admin"));
+const StudentContent = React.lazy(() => import("../pages/student/Student"));
+const NotFound = React.lazy(
+  () => import("../layouts/common/components/NotFound")
+);
 
 const RouterComponent = () => {
-  const userSession = secureLocalStorage.getItem('userSession') || {};
+  const userSession = secureLocalStorage.getItem("userSession") || {};
   const roleId = userSession.roleId;
 
   return (
@@ -23,7 +25,10 @@ const RouterComponent = () => {
           <Route path="/" element={<Home />} />
           <Route path="/explore" element={<Explore />} />
           <Route path="/explore/description" element={<EventDescription />} />
-          <Route path="/explore/event-details/:eventId" element={<EventDescription />} />
+          <Route
+            path="/explore/event-details/:eventId"
+            element={<EventDescription />}
+          />
 
           <Route
             path="/mentor/*"
